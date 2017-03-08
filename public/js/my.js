e046f386cedab38c0e8b61034f2b10750ce7ec7c
skyran1278 <skyran1278@gmail.com>
2017-03-08 20:10:35 +0800

    V5

--- public/js/my.js
+++ public/js/my.js
@@ -9,7 +9,7 @@ var run = function() {;
     .attr('stroke', 'green')
     .attr('stroke-width', 4)
     .attr('fill', 'yellow');
-    document.querySelector('#introduce').innerHTML = '<h4>目前沒有資料喔！</h4>'
+    document.querySelector('#introduce').innerHTML = '目前沒有資料喔！'
 };
 
 
